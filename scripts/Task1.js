2da84b2c3f6567f55fa6f431e875cdaa4b102bf8
saleha <saleha.zf22@gmail.com>
2024-11-17 12:57:32 +0500

    some modifications

---
+++ scripts/Task1.js
@@ -0,0 +1,14 @@
+// Show Details
+document.getElementById("showDetails").addEventListener("click", function () {
+    document.getElementById("detailsSection").style.display = "block";
+});
+
+// Hide Details
+document.getElementById("hideDetails").addEventListener("click", function () {
+    document.getElementById("detailsSection").style.display = "none";
+});
+
+// Change Background Color
+document.getElementById("changeColor").addEventListener("click", function () {
+    document.body.style.backgroundColor = "#70d0cc"; // Bright teal color
+});
