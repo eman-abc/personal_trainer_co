682f17c2904aee8efbed4b303b7367ab51b96448
saleha <saleha.zf22@gmail.com>
2024-11-17 13:28:39 +0500

    some modifications

--- scripts/rollover.js
+++ scripts/rollover.js
@@ -39,9 +39,64 @@ window.onload = function () {
     "images/service images/sports/triathlonHover.jpg",
     "images/service images/sports/triathlon.jpg"
   );
+
+  addRolloverEffect(
+    "trainerImage",
+    "images/Rollover images/RebaSmith-Rollover.jpg",
+    "images/service images/yoga/RebaSmith.jpg"
+  );
+
+
   addRolloverEffect(
     "volleyballImage",
     "images/service images/sports/volleyballHover.jpg",
     "images/service images/sports/volleyball.jpg"
   );
+
+  addRolloverEffect(
+    "blogImage1",
+    "images/blogimage1hover.jpg",
+    "images/blogimage1.jpeg"
+  );
+
+
+  addRolloverEffect(
+    "blogImage2",
+    "images/blogimage2hover.jpg",
+    "images/blogimage2.jpg"
+  );
+
+  addRolloverEffect(
+    "blogImage3",
+    "'images/blogimage3hover.jpg",
+    "images/blogimage3.jpg"
+  );
+
+  addRolloverEffect(
+    "blogImage4",
+    "images/blogimage4hover.jpg",
+    "images/blogimage1.jpeg"
+  );
+
+  addRolloverEffect(
+    "blogImage5",
+    "images/blogimage5hover.jpg",
+    "images/blogimage5.jpg"
+  );
+
+  addRolloverEffect(
+    "blogImage6",
+    "images/blogimage6hover.jpg",
+    "images/blogimage6.webp"
+  );
+
+  
+
+
 };
+
+
+
+
+
+
